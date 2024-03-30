af243aa8942b8e9689f1873266b5ac2fcd379b7f
Nick <nandrievsky@gmail.com>
2024-03-30 18:11:51 +0100

    refactoring, move pairing into the heap package

--- heap/pairing.go
+++ heap/pairing.go
@@ -1,4 +1,4 @@
-package ph
+package heap
 
 import "cmp"
 
@@ -44,4 +44,30 @@ func mergePairs[Elem cmp.Ordered](trees []*PairingTree[Elem]) *PairingTree[Elem]
 		return trees[0]
 	}
 	return Meld(Meld(trees[0], trees[1]), mergePairs(trees[2:]))
+}
+
+type PairingHeap[Elem cmp.Ordered] struct {
+	root *PairingTree[Elem]
+}
+
+func New[Elem cmp.Ordered]() *PairingHeap[Elem] {
+	return &PairingHeap[Elem]{nil}
+}
+
+func (heap *PairingHeap[Elem]) Insert(e Elem) {
+	heap.root = Meld(heap.root, &PairingTree[Elem]{e, nil})
+}
+
+func (heap *PairingHeap[Elem]) Peek() Elem {
+	return heap.root.head
+}
+
+func (heap *PairingHeap[Elem]) Pop() Elem {
+	e := heap.root.head
+	heap.root = mergePairs(heap.root.tail)
+	return e
+}
+
+func (heap *PairingHeap[Elem]) IsEmpty() bool {
+	return heap.root == nil
 }
--- heap/pairing_test.go
+++ heap/pairing_test.go
@@ -1,4 +1,4 @@
-package ph
+package heap
 
 import (
 	"testing"
@@ -69,6 +69,79 @@ func TestPairingTreeIsEmpty2(t *testing.T) {
 	_, heap = Pop(heap)
 	_, heap = Pop(heap)
 	actual = IsEmpty(heap)
+	expected = true
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestPairingHeapPeek(t *testing.T) {
+	heap := New[int]()
+	heap.Insert(1)
+	actual := heap.Peek()
+	expected := 1
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestPairingHeapPeek2(t *testing.T) {
+	heap := New[int]()
+	heap.Insert(1)
+	heap.Insert(2)
+	actual := heap.Peek()
+	expected := 2
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestPairingHeapPop(t *testing.T) {
+	heap := New[int]()
+	heap.Insert(1)
+	actual := heap.Pop()
+	expected := 1
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestPairingHeapPop2(t *testing.T) {
+	heap := New[int]()
+	heap.Insert(1)
+	heap.Insert(2)
+	actual := heap.Pop()
+	expected := 2
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+	actual = heap.Pop()
+	expected = 1
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestPairingHeapIsEmpty(t *testing.T) {
+	heap := New[int]()
+	actual := heap.IsEmpty()
+	expected := true
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+
+func TestPairingHeapIsEmpty2(t *testing.T) {
+	heap := New[int]()
+	heap.Insert(1)
+	actual := heap.IsEmpty()
+	expected := false
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+
+	heap.Pop()
+	actual = heap.IsEmpty()
 	expected = true
 	if actual != expected {
 		t.Errorf("got %v\nwant %v", actual, expected)
